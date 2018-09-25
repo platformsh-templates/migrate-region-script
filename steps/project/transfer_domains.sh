0550d5782854cf5c31bdebae740a403617276871
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2018-09-25 11:11:56 -0400

    transfer_domains

---
+++ steps/project/transfer_domains.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+source common/functions.sh
+
+check_project_ids
+
+from_id=$(get_from_project_id)
+to_id=$(get_to_project_id)
+
+domains=$(platform domain:list --project=$from_id --format tsv 2>/dev/null | tail -n +2 | awk '{print $1}')
+to_edge_host=$(platform environment:info --project=$to_id --environment=master -- edge_hostname)
+if [ -z "$domains" ] ; then
+    echo "[ERROR] No domains are assigned to \"$to_id\" platform.sh project"
+    exit 1
+fi
+
+list=""
+for domain in $domains;
+do
+    list=$(printf "$list\n- $domain")
+done
+message=$(printf "Please point following domain(s):$list\nto\n$to_edge_host\nConfirm")
+confirm_message "$message" false
+
+for domain in $domains;
+do
+    platform domain:delete --project=$from_id $domain --yes
+    platform domain:add --project=$to_id $domain --yes
+done
