636d42e79ba43555e59c508f47f27ba70fbfcfb1
Florent Huck <flovntp@gmail.com>
2023-02-23 15:49:05 +0100

    solving issue with DB import

--- migrate.sh
+++ migrate.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "> Enter project ID to migrate:"
-printf ": "
+printf "> Enter project ID to migrate:"
+echo ": "
 read P1_PROJECT_ID
 
-echo "\n> The region where the new project will be hosted
+printf "\n> The region where the new project will be hosted
 [au.platform.sh  ] Sydney, Australia (AWS) [867 gC02eq/kWh]
 [au-2.platform.sh] Sydney, Australia (AZURE) [867 gC02eq/kWh]
 [ca-1.platform.sh] Montreal, Canada (AWS) [27 gC02eq/kWh]
@@ -18,23 +18,23 @@ echo "\n> The region where the new project will be hosted
 [us-2.platform.sh] Washington, United States (AWS) [514 gC02eq/kWh]
 [us-3.platform.sh] Moses Lake, United States (AZURE) [24 gC02eq/kWh]
 [us-4.platform.sh] Charleston, United States (GCP) [480 gC02eq/kWh]"
-printf ": "
+echo ": "
 read REGION
 
-echo "\n> On which organization do you want the new project to be?"
+printf "\n> On which organization do you want the new project to be?\n"
 platform organization:list --columns=name --no-header
 printf ": "
 read ORGANIZATION
 
 P1_DEFAULT_BRANCH=$(platform project:info -p $P1_PROJECT_ID default_branch)
 
-echo "\n> Please provide the app that contains the database to migrate (ex: app)"
+printf "\n> Please provide the app that contains the database to migrate (ex: app)\n"
 platform app:list -p $P1_PROJECT_ID -e $P1_DEFAULT_BRANCH --no-header --columns=name
-printf ": "
+echo ": "
 read DATABASE_APP
 
-echo "\n> Please provide a valid Github API Token:"
-printf ": "
+printf "\n> Please provide a valid Github API Token:"
+echo ": "
 read GITHUB_API_TOKEN
 
 # get project P1 name
@@ -53,31 +53,12 @@ printf "\nProject plan is $P1_PLAN\n"
 #   - the same env name for production env (default env)
 #   - the same plan
 P2_PROJECT_ID=$(platform project:create --title="$P1_NAME" --region=$REGION --default-branch="$P1_DEFAULT_BRANCH" --environments=21 --no-interaction --org=$ORGANIZATION --plan=$P1_PLAN)
-printf "\nNew project ID is $P2_PROJECT_ID"
-
-# get P1 integration repo
-P1_INTEGRATION_REPO=$(platform project:curl -p $P1_PROJECT_ID /integrations | jq -r '.[]|select(.type | contains("github"))' | jq '.repository'| tr -d '"')
-P1_INTEGRATION_ID=$(platform project:curl -p $P1_PROJECT_ID /integrations | jq -r '.[]|select(.type | contains("github"))' | jq '.id')
-printf "\nP1 Integration repo is $P1_INTEGRATION_REPO and ID is $P1_INTEGRATION_ID"
-printf $P1_INTEGRATION_REPO
-printf "\n"
-
-# remove P1 integration
-#TODO uncomment if you need to remove the integration on P1 project
-# REMOVE_INTEGRATION=$(platform integration:delete -p $P1_PROJECT_ID --yes $P1_INTEGRATION_ID)
-
-# create integration (github api token) on P2 project and catch errors
-(
-  set -e
-  INTEGRATION=$(platform integration:add --type=github --project=$P2_PROJECT_ID --repository=$P1_INTEGRATION_REPO --token=$GITHUB_API_TOKEN --no-interaction -vvv)
-  printf "\nplatform integration:add --type=github --project=$P2_PROJECT_ID --repository=$P1_INTEGRATION_REPO --token=$GITHUB_API_TOKEN --no-interaction -vvv"
-  printf "\nP2 Integration is $INTEGRATION"
-)
 errorCode=$?
 if [ $errorCode -ne 0 ]; then
-  echo "We have an error"
-  printf "\nP2 Integration already exists"
+  printf "\nNew project ID is empty, an error occurs during project creation"
+  exit
 fi
+printf "\nNew project ID is $P2_PROJECT_ID\n"
 
 # get P1 project envs
 ENV_LIST=$(platform environment:list -p $P1_PROJECT_ID --pipe)
@@ -89,21 +70,50 @@ steps/set_projects.sh $P1_PROJECT_ID $P2_PROJECT_ID
 steps/copy_project.sh
 
 for ENV in "${P1_ENVS[@]}"; do
-    printf "\nCopy env $ENV"
-    steps/copy_environment.sh $ENV
 
+    printf "platform project:curl -p $P1_PROJECT_ID /environments/$ENV | jq -r '.status'"
     ENV_CHECK=$(platform project:curl -p $P1_PROJECT_ID /environments/$ENV | jq -r '.status')
+
     if [ "$ENV_CHECK" = active ]; then
+      printf "\nCopy env $ENV\n"
+      steps/copy_environment.sh $ENV
       printf "\nCopy data for env $ENV\n"
-      # Wait 1 minute, just to be sure new env is up and running
-      sleep 60s
+      # Wait 1:30 minute, just to be sure new env is up and running
+      sleep 120s
       steps/copy_data.sh $ENV $DATABASE_APP
     else
       printf "\nEnv $ENV is not active, skip copy data\n"
     fi
 done
 
-read -p "Would you like to transfer domains now (y/n)?: " choice
-case "$choice" in
-  y|Y ) steps/project/transfer_domains.sh;
-esac
+# get P1 integration repo
+P1_INTEGRATION_REPO=$(platform project:curl -p $P1_PROJECT_ID /integrations | jq -r '.[]|select(.type | contains("github"))' | jq '.repository'| tr -d '"')
+P1_INTEGRATION_ID=$(platform project:curl -p $P1_PROJECT_ID /integrations | jq -r '.[]|select(.type | contains("github"))' | jq '.id')
+printf "\nP1 Integration repo is $P1_INTEGRATION_REPO and ID is $P1_INTEGRATION_ID"
+printf "\n"
+
+# remove P1 integration
+#TODO uncomment if you need to remove the integration on P1 project
+# REMOVE_INTEGRATION=$(platform integration:delete -p $P1_PROJECT_ID --yes $P1_INTEGRATION_ID)
+
+# create integration (github api token) on P2 project and catch errors
+(
+  set -e
+  printf "\nplatform integration:add --type=github --project=$P2_PROJECT_ID --repository=$P1_INTEGRATION_REPO --token=$GITHUB_API_TOKEN --no-interaction\n"
+  INTEGRATION=$(platform integration:add --type=github --project=$P2_PROJECT_ID --repository=$P1_INTEGRATION_REPO --token=$GITHUB_API_TOKEN --no-interaction)
+  printf "\nP2 Integration is $INTEGRATION\n"
+)
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+  echo "We have an error"
+  printf "\nP2 Integration already exists\n"
+fi
+
+
+printf "\nNew project ID is $P2_PROJECT_ID"
+printf "\nNew project console url is https://console.platform.sh/$REGION/$P2_PROJECT_ID\n"
+
+#read -p "Would you like to transfer domains now (y/n)?: " choice
+#case "$choice" in
+#  y|Y ) steps/project/transfer_domains.sh;
+#esac
--- steps/environment/data/copy_db.sh
+++ steps/environment/data/copy_db.sh
@@ -11,5 +11,44 @@ app=$2
 
 # Database
 mkdir -p .local/tmp
+printf "\nbefore dump\n"
+printf "platform db:dump --project=$from_id --environment=$env --file=.local/tmp/dump.sql --yes --app=$app\n"
 platform db:dump --project=$from_id --environment=$env --file=.local/tmp/dump.sql --yes --app=$app
+
+printf "\nbefore import\n"
+
+now=$(date)
+printf "%s\n" "$now --> refresh env:info"
+platform env:info -p $to_id --refresh
+#
+#printf "platform env:list -p $to_id"
+#platform env:list -p $to_id
+#
+#printf "\nplatform project:curl -p $to_id /environments/$env | jq -r '.status'\n"
+#ENV_CHECK=$(platform project:curl -p $to_id /environments/$env | jq -r '.status')
+#printf "\nEnv $env from project $to_id is $ENV_CHECK\n"
+#
+#printf "\nPlatform ssh list platform ssh -p $to_id --all\n"
+#platform ssh -p $to_id --all
+#
+#printf "\n$(platform ssh -p cmu7r7fhyuplg --all)\n"
+#until [ $(platform ssh -p cmu7r7fhyuplg --all | wc -l) > 0 ]
+#do
+#     printf "wait 5"
+#     sleep 5s
+#done
+#printf "\nUrl founded\n"
+#platform ssh -p $to_id --all
+#
+#printf "\nPlatform activities platform act -p $to_id \n"
+#platform act -p $to_id
+#
+#printf "\nEnvironment $env info\n"
+#platform env:info -p $to_id -e $env
+
+printf "\nTest ssh connection with ls -l\n"
+platform ssh -p $to_id -e $env -- ls -l
+
+printf "\nplatform db:sql --project=$to_id --environment=$env < .local/tmp/dump.sql\n"
 platform db:sql --project=$to_id --environment=$env --app=$app < .local/tmp/dump.sql
+printf "\nEnd copy DB\n"
